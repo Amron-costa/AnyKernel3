17cb98370ea54d9d912e35a746df05ec93c48d25
Amron Costa <70508199+Amron-costa@users.noreply.github.com>
2020-11-10 21:51:13 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string= AVICII  by @Amron_Costa and @Skybird350 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
